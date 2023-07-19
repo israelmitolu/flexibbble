0bb402aae03a2b2ed99fe9985bd5b9b75b6d8339
israelmitolu <israelmitolu@gmail.com>
2023-07-19 06:25:54 +0100

    Define authentication and authorization

---
+++ lib/actions.ts
@@ -0,0 +1,41 @@
+import { createUserMutation, getUserQuery } from "@/graphql";
+import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  : "http://127.0.0.1:4000/graphql";
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  : "letmein";
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
+
+const client = new GraphQLClient(apiUrl);
+
+const makeGraphqlRequest = async (query: string, variables = {}) => {
+  try {
+    return await client.request(query, variables);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUser = async (email: string) => {
+  client.setHeader("x-api-key", apiKey);
+  return makeGraphqlRequest(getUserQuery, { email });
+};
+
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+  client.setHeader("x-api-key", apiKey);
+  const variables = {
+    input: {
+      name,
+      email,
+      avatarUrl,
+    },
+  };
+
+  return makeGraphqlRequest(createUserMutation, variables);
+};
