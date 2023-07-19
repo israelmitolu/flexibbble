9f5020227e331dd6e3653adf0be4b80b4d15e056
israelmitolu <israelmitolu@gmail.com>
2023-07-19 06:29:27 +0100

    Create Profile Menu component with Headless UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,46 @@
-import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import AuthProviders from "./AuthProviders";
 
-const Navbar = () => {
-  const session = {};
+import { NavLinks } from "@/constants";
+import { getCurrentUser } from "@/lib/session";
+
+import AuthProviders from "./AuthProviders";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
+// import Button from "./Button";
+// import ProfileMenu from "./ProfileMenu";
+
+const Navbar = async () => {
+  const session = await getCurrentUser();
+
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
         <Link href="/">
-          <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
+          <Image src="/logo.svg" width={116} height={43} alt="logo" />
         </Link>
-
-        {/* Desktop Nav */}
         <ul className="xl:flex hidden text-small gap-7">
           {NavLinks.map((link) => (
-            <Link href={link.href} key={link.key}>
+            <Link href={link.href} key={link.text}>
               {link.text}
             </Link>
           ))}
         </ul>
+      </div>
 
-        <div className="flexCenter gap-4">
-          {session ? (
-            <>
-              User Photo
-              <Link href="/create-project">Share Work</Link>
-            </>
-          ) : (
-            <AuthProviders />
-          )}
-        </div>
+      <div className="flexCenter gap-4">
+        {session?.user ? (
+          <>
+            <ProfileMenu session={session} />
+
+            <Link href="/create-project">
+              {/* <Button title="Share work" /> */}
+              Share work
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
